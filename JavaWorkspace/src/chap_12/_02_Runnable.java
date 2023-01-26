bc7ff96d13a850e39d287bd4850104a0db7f2ddd
howecofe <am0229@naver.com>
2023-01-26 21:09:21 +0900

    12. 쓰레드

---
+++ JavaWorkspace/src/chap_12/_02_Runnable.java
@@ -0,0 +1,25 @@
+package chap_12;
+
+import chap_12.clean.CleanRunnable;
+
+public class _02_Runnable {
+    // Thread: Thread 라는 클래스를 상속
+    // Runnable: Runnable 이라는 인터페이스를 구현
+    // -> 차이 : 상속은 단일 상속만 가능. 인터페이스는 다중 상속 가능.
+    public static void main(String[] args) {
+        // Runnable
+        CleanRunnable cleanRunnable = new CleanRunnable();
+        Thread thread = new Thread(cleanRunnable);
+        thread.start();
+
+        cleanByBoss();
+    }
+
+    public static void cleanByBoss() {
+        System.out.println("-- 사장 청소 시작 --");
+        for (int i = 1; i <= 10 ; i += 2) {
+            System.out.println("(사장) " + i + "번방 청소 중");
+        }
+        System.out.println("-- 사장 청소 끝 --");
+    }
+}
