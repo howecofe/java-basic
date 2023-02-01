7d57678e5bae1882e8ec5e69fc6f626ce66cdfc1
howecofe <am0229@naver.com>
2023-02-01 16:46:48 +0900

    상속 관련 주석 수정

--- JavaWorkspace/src/chap_12/_02_Runnable.java
+++ JavaWorkspace/src/chap_12/_02_Runnable.java
@@ -5,7 +5,7 @@ import chap_12.clean.CleanRunnable;
 public class _02_Runnable {
     // Thread: Thread 라는 클래스를 상속
     // Runnable: Runnable 이라는 인터페이스를 구현
-    // -> 차이 : 상속은 단일 상속만 가능. 인터페이스는 다중 상속 가능.
+    // -> 차이 : 클래스에서의 상속은 단일 상속만 가능. 인터페이스는 다중 상속 가능.
     public static void main(String[] args) {
         // Runnable
         CleanRunnable cleanRunnable = new CleanRunnable();
